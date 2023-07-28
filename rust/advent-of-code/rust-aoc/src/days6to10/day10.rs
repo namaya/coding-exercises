064f70228b95efd13509bd8939e51ccaf2c67eba
namaya <n3amaya@gmail.com>
2023-07-28 17:16:29 -0500

    passed 2022 aoc day10-1

--- rust/advent-of-code/rust-aoc/src/days6to10/day10.rs
+++ rust/advent-of-code/rust-aoc/src/days6to10/day10.rs
@@ -2,55 +2,40 @@
 use std::fs;
 
 pub fn day10_cathode_ray_tube() {
-    let mut signal_history: [i32; 250] = [0; 250];
-    signal_history[0] = 1;
+    let mut v = 1;
+    let mut v_hist: Vec<i32> = vec![0];
 
-    let mut cycle_num: usize = 0;
+    let input_path = "inputs/2022/day10/full.txt";
 
-    let input_path = "inputs/2022/day10/example.txt";
+    let contents = fs::read_to_string(input_path)
+        .expect("invalid input path");
 
-    fs::read_to_string(input_path)
-        .expect("invalid input path")
-        .split("\n")
-        .map_while(|instruction| {
-            let instruction = instruction.split_whitespace()
-                .collect::<Vec<&str>>();
 
-            match instruction[0] {
-                "addx" => {
-                    if cycle_num + 1 < signal_history.len() {
-                        signal_history[cycle_num+1] = signal_history[cycle_num];
-                    }
+    for instruction in contents.lines() {
+        let instruction: Vec<&str> = instruction.split_whitespace().collect();
 
-                    if cycle_num + 2 < signal_history.len() {
-                        signal_history[cycle_num+2] = signal_history[cycle_num] + instruction[1].parse::<i32>().expect("invalid addx argument.");
-                    }
+        match instruction[0] {
+            "addx" => {
+                v_hist.push(v);
+                v_hist.push(v);
 
-                    cycle_num += 2;
-                },
-                "noop" => {
-                    signal_history[cycle_num+1] = signal_history[cycle_num];
-                    cycle_num += 1;
-                },
-                _ => panic!("Unknown command."),
-            }
+                v += instruction[1].parse::<i32>()
+                    .expect("invalid arg to addx.");
+            },
+            "noop" => {
+                v_hist.push(v);
+            },
+            _ => panic!("Unkown command"),
+        }
+    }
 
-            if cycle_num >= signal_history.len() {
-                None
-            } else {
-                Some(())
-            }
-        })
-        .for_each(|_| {});
-    
-    signal_history.iter().enumerate().map(|(i, sig)| format!("{}: {}", i, sig))
-        .for_each(|s| println!("{}", s));
+    v_hist.push(v);
 
-    let sigs: Vec<i32> = signal_history.iter()
+    let answer = v_hist.iter()
         .enumerate()
-        .filter(|(i, _)| (i) % 40 == 20 )
-        .map(|(i, signal)| (i as i32) * signal )
-        .collect();
+        .filter(|(i, _)| i % 40 == 20)
+        .map(|(i, v)|  (i as i32) * v)
+        .sum::<i32>();
 
-    println!("{:?}", sigs);
+    println!("{:?}", answer);
 }
