12b1ee1aa3b5b3808f159b27baa8e590a9bf28f5
namaya <n3amaya@gmail.com>
2023-12-11 07:48:49 -0600

    feat(dayx_template.cpp): add part1 and part2 functions.

--- cpp/aoc/dayx_template.cpp
+++ cpp/aoc/dayx_template.cpp
@@ -2,7 +2,9 @@
 #include <iostream>
 #include <vector>
 
-std::vector<std::string> parse_input(std::istream &input_stream);
+std::vector<std::string> parse_input(std::istream &);
+int part1(std::vector<std::string>);
+int part2(std::vector<std::string>);
 
 int main(int argc, char *argv[]) {
   if (argc > 2) {
@@ -19,8 +21,14 @@ int main(int argc, char *argv[]) {
     exit(1);
   }
 
-  // FILL ME IN.
+  auto input = parse_input(input_file);
+
+  std::cout << "Part1: " << part1(input) << "\n";
+  std::cout << "Part2: " << part2(input) << "\n";
 }
+
+int part1(std::vector<std::string> input) { return 0; }
+int part2(std::vector<std::string> input) { return 0; }
 
 std::vector<std::string> parse_input(std::istream &input_stream) {
   auto result = std::vector<std::string>{};
